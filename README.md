87abb50b401fb43e19a02a7ac36781407abf2d40
Luke Stephenson <luke.stephenson@rea-group.com>
2018-05-02 10:58:52 +1000

    Addition of first tests

---
+++ README.md
@@ -0,0 +1,18 @@
+# monix-adventures
+In general you should complete the exercises in the master branch.
+There is a solution branch with  ... guess what...
+
+Choose your adventure below
+
+
+## Adventure 1: Aysnchrony with monix Task
+The aim is:
+
+1. Gain practice/confidence in working with monix Task.
+
+Complete the exercises in: adventures.task.TaskAventures
+
+There are a series of tests that you need to make pass in TaskAventuresSpec.
+Run them with `sbt "testOnly *.TaskAventuresSpec"`.  Tests can also be run in the IDE.
+
+
---
+++ src/test/scala/adventures/task/TaskAdventuresSpec.scala
@@ -0,0 +1,87 @@
+package adventures.task
+
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import monix.execution.schedulers.TestScheduler
+import org.specs2.matcher.Scope
+import org.specs2.mutable.Specification
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class TaskAdventuresSpec extends Specification {
+
+  "TaskAdventuresSpec" should {
+    "immediately executing Task should return 43" in {
+      result(TaskAdventures.immediatelyExecutingTask()) must beEqualTo(43)
+    }
+
+    "side effectful helloWorld Task" should {
+      "have no side effect if not executed" in new LoggingScope {
+        TaskAdventures.helloWorld(logger)
+
+        logged.toList must beEmpty
+      }
+
+      "have single side effect if run once" in new LoggingScope {
+        result(TaskAdventures.helloWorld(logger))
+
+        logged.toList must beEqualTo(List("hello world"))
+      }
+
+      "have two side effects if run twice" in new LoggingScope {
+        val helloWorldTask = TaskAdventures.helloWorld(logger)
+        result(helloWorldTask)
+        result(helloWorldTask)
+
+        logged.toList must beEqualTo(List("hello world", "hello world"))
+      }
+    }
+
+    "failing Task should fail" in {
+      result(TaskAdventures.alwaysFailingTask()) must throwA[Exception]()
+    }
+
+    "get Current Temp In F" in {
+      result(TaskAdventures.getCurrentTempInF(() => Task(45))) must beEqualTo(113)
+    }
+
+    "get Current Temp In F again" in {
+      def currentTemp() = Task(45)
+
+      def cToF(c: Int) = Task {
+        c * 9/5 + 32
+      }
+
+      result(TaskAdventures.getCurrentTempInFAgain(currentTemp, cToF)) must beEqualTo(113)
+    }
+
+    "calculate string complexity in parallel" in {
+      val source = List("a", "b", "c")
+
+      def complexity(string: String) = Task(string.length).delayExecution(1.second)
+
+      val task: Task[Int] = TaskAdventures.calculateStringComplexityInParallel(source, complexity)
+
+      val scheduler = TestScheduler()
+
+      val result = task.runAsync(scheduler)
+
+      scheduler.tick(1.second)
+
+      Await.result(result, 2.seconds) must beEqualTo(3)
+    }
+
+  }
+
+  class LoggingScope extends Scope {
+    val logged = ListBuffer[String]()
+
+    def logger(log: String): Unit = logged.append(log)
+  }
+
+  private def result[T](task: Task[T]): T = {
+    Await.result(task.runAsync, 2.seconds)
+  }
+}
